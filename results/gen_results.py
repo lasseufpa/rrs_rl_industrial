a0c18b47d2eeac24cb0c143ea8896d2c75cad6b8
Cleverson Nahum <cleversonahum@gmail.com>
2023-04-27 19:19:59 -0300

    Plotting different agent results in the same graph

--- results/gen_results.py
+++ results/gen_results.py
@@ -7,64 +7,72 @@ import numpy as np
 
 
 def gen_results(
-    scenario_name: str,
-    agent_name: str,
+    scenario_names: list[str],
+    agent_names: list[str],
     episodes: np.ndarray,
     metrics: list,
     slices: np.ndarray,
 ):
-    for episode in episodes:
-        data = np.load(
-            f"hist/{scenario_name}/{agent_name}/ep_{episode}.npz",
-            allow_pickle=True,
-        )
-        data_metrics = {
-            "pkt_incoming": data["pkt_incoming"],
-            "pkt_throughputs": data["pkt_throughputs"],
-            "pkt_effective_thr": data["pkt_effective_thr"],
-            "buffer_occupancies": data["buffer_occupancies"],
-            "buffer_latencies": data["buffer_latencies"],
-            "dropped_pkts": data["dropped_pkts"],
-            "mobility": data["mobility"],
-            "spectral_efficiencies": data["spectral_efficiencies"],
-            "basestation_ue_assoc": data["basestation_ue_assoc"],
-            "basestation_slice_assoc": data["basestation_slice_assoc"],
-            "slice_ue_assoc": data["slice_ue_assoc"],
-            "sched_decision": data["sched_decision"],
-            "reward": data["reward"],
-            "slice_req": data["slice_req"],
-        }
-
-        for metric in metrics:
-            plt.figure()
-            w, h = matfig.figaspect(0.6)
-            plt.figure(figsize=(w, h))
-            (xlabel, ylabel) = plot_graph(data_metrics, metric, slices)
-            plt.grid()
-            plt.xlabel(xlabel, fontsize=14)
-            plt.ylabel(ylabel, fontsize=14)
-            plt.xticks(fontsize=12)
-            plt.legend(fontsize=12)
-            os.makedirs(
-                f"./results/{scenario_name}/{agent_name}/ep_{episode}",
-                exist_ok=True,
-            )
-            plt.savefig(
-                "./results/{}/{}/ep_{}/{}.pdf".format(
-                    scenario_name, agent_name, episode, metric
-                ),
-                bbox_inches="tight",
-                pad_inches=0,
-                format="pdf",
-                dpi=1000,
-            )
-            plt.close()
+    xlabel = ylabel = ""
+    for scenario in scenario_names:
+        for episode in episodes:
+            for metric in metrics:
+                plt.figure()
+                w, h = matfig.figaspect(0.6)
+                plt.figure(figsize=(w, h))
+                for agent in agent_names:
+                    (xlabel, ylabel) = plot_graph(
+                        metric, slices, agent, scenario, episode
+                    )
+                plt.grid()
+                plt.xlabel(xlabel, fontsize=14)
+                plt.ylabel(ylabel, fontsize=14)
+                plt.xticks(fontsize=12)
+                plt.legend(fontsize=12)
+                os.makedirs(
+                    f"./results/{scenario}/ep_{episode}",
+                    exist_ok=True,
+                )
+                plt.savefig(
+                    "./results/{}/ep_{}/{}.pdf".format(
+                        scenario, episode, metric
+                    ),
+                    bbox_inches="tight",
+                    pad_inches=0,
+                    format="pdf",
+                    dpi=1000,
+                )
+                plt.close()
 
 
 def plot_graph(
-    data_metrics: dict, metric: str, slices: np.ndarray
+    metric: str,
+    slices: np.ndarray,
+    agent: str,
+    scenario: str,
+    episode: int,
 ) -> Tuple[str, str]:
     xlabel = ylabel = ""
+    data = np.load(
+        f"hist/{scenario}/{agent}/ep_{episode}.npz",
+        allow_pickle=True,
+    )
+    data_metrics = {
+        "pkt_incoming": data["pkt_incoming"],
+        "pkt_throughputs": data["pkt_throughputs"],
+        "pkt_effective_thr": data["pkt_effective_thr"],
+        "buffer_occupancies": data["buffer_occupancies"],
+        "buffer_latencies": data["buffer_latencies"],
+        "dropped_pkts": data["dropped_pkts"],
+        "mobility": data["mobility"],
+        "spectral_efficiencies": data["spectral_efficiencies"],
+        "basestation_ue_assoc": data["basestation_ue_assoc"],
+        "basestation_slice_assoc": data["basestation_slice_assoc"],
+        "slice_ue_assoc": data["slice_ue_assoc"],
+        "sched_decision": data["sched_decision"],
+        "reward": data["reward"],
+        "slice_req": data["slice_req"],
+    }
     for slice in slices:
         match metric:
             case (
@@ -76,14 +84,17 @@ def plot_graph(
                 slice_throughput = calc_throughput_slice(
                     data_metrics, metric, slice
                 )
-                plt.plot(slice_throughput, label=f"Slice {slice}")
+                plt.plot(slice_throughput, label=f"{agent}, slice {slice}")
                 xlabel = "Step (n)"
                 ylabel = "Throughput (Mbps)"
             case ("buffer_latencies" | "buffer_occupancies"):
                 avg_spectral_efficiency = calc_slice_average(
                     data_metrics, metric, slice
                 )
-                plt.plot(avg_spectral_efficiency, label=f"Slice {slice}")
+                plt.plot(
+                    avg_spectral_efficiency,
+                    label=f"{agent}, slice {slice}",
+                )
                 xlabel = "Step (n)"
                 match metric:
                     case "buffer_latencies":
@@ -94,7 +105,7 @@ def plot_graph(
                 number_elements = np.sum(
                     np.sum(data_metrics[metric], axis=2), axis=1
                 )
-                plt.plot(number_elements)
+                plt.plot(number_elements, label=f"{agent}")
                 xlabel = "Step (n)"
                 match metric:
                     case "basestation_ue_assoc":
@@ -106,16 +117,18 @@ def plot_graph(
                 number_uers_per_slice = np.sum(
                     data_metrics[metric][:, slice, :], axis=1
                 )
-                plt.plot(number_uers_per_slice, label=f"Slice {slice}")
+                plt.plot(
+                    number_uers_per_slice, label=f"{agent}, slice {slice}"
+                )
                 xlabel = "Step (n)"
                 ylabel = "Number of UEs"
             case "reward":
-                plt.plot(data_metrics[metric])
+                plt.plot(data_metrics[metric], label=f"{agent}")
                 xlabel = "Step (n)"
                 ylabel = "Reward"
                 break
             case "reward_cumsum":
-                plt.plot(np.cumsum(data_metrics["reward"]))
+                plt.plot(np.cumsum(data_metrics["reward"]), label=f"{agent}")
                 xlabel = "Step (n)"
                 ylabel = "Cumulative reward"
                 break
@@ -123,9 +136,10 @@ def plot_graph(
                 total_throughput = calc_total_throughput(
                     data_metrics, "pkt_throughputs"
                 )
-                plt.plot(total_throughput)
+                plt.plot(total_throughput, label=f"{agent}")
                 xlabel = "Step (n)"
                 ylabel = "Throughput (Mbps)"
+                break
             case "spectral_efficiencies":
                 if slice not in [11]:
                     slice_ues = data_metrics["slice_ue_assoc"][:, slice, :]
@@ -146,28 +160,43 @@ def plot_graph(
                             np.isclose(den, np.zeros_like(den))
                         ),
                     )
-                    plt.plot(spectral_eff, label=f"Slice {slice}")
+                    plt.plot(spectral_eff, label=f"{agent}, slice {slice}")
                     xlabel = "Step (n)"
                     ylabel = "Thoughput capacity per RB (Mbps)"
             case "violations":
                 violations = calc_slice_violations(data_metrics)
-                plt.plot(np.sum(violations[:1, :], axis=0), label="Slice 0")
-                plt.plot(np.sum(violations[2:4, :], axis=0), label="Slice 1")
-                plt.plot(violations[4, :], label="Slice 2")
-                plt.plot(np.sum(violations, axis=0), label="Total")
+                plt.plot(
+                    np.sum(violations[:1, :], axis=0),
+                    label=f"{agent}, slice 0",
+                )
+                plt.plot(
+                    np.sum(violations[2:4, :], axis=0),
+                    label=f"{agent}, slice 1",
+                )
+                plt.plot(violations[4, :], label=f"{agent}, slice 2")
+                plt.plot(np.sum(violations, axis=0), label=f"{agent}, total")
+                xlabel = "Step (n)"
+                ylabel = "# Violations"
                 break
             case "violations_cumsum":
                 violations = calc_slice_violations(data_metrics)
                 plt.plot(
                     np.cumsum(np.sum(violations[:1, :], axis=0)),
-                    label="Slice 0",
+                    label=f"{agent}, slice 0",
                 )
                 plt.plot(
                     np.cumsum(np.sum(violations[2:4, :], axis=0)),
-                    label="Slice 1",
+                    label=f"{agent}, slice 1",
                 )
-                plt.plot(np.cumsum(violations[4, :]), label="Slice 2")
-                plt.plot(np.cumsum(np.sum(violations, axis=0)), label="Total")
+                plt.plot(
+                    np.cumsum(violations[4, :]), label=f"{agent}, slice 2"
+                )
+                plt.plot(
+                    np.cumsum(np.sum(violations, axis=0)),
+                    label=f"{agent}, total",
+                )
+                xlabel = "Step (n)"
+                ylabel = "Cumulative # violations"
                 break
             case _:
                 raise Exception("Metric not found")
@@ -270,27 +299,26 @@ def calc_slice_violations(data_metrics) -> np.ndarray:
     return violations
 
 
-scenario_name = "industrial"
-agent_name = "ssr_protect"  # "ssr_protect"
-# metrics = [
-#     "pkt_incoming",
-#     "pkt_effective_thr",
-#     "pkt_throughputs",
-#     "dropped_pkts",
-#     "buffer_occupancies",
-#     "buffer_latencies",
-#     "basestation_ue_assoc",
-#     "basestation_slice_assoc",
-#     "slice_ue_assoc",
-#     "reward",
-#     "reward_cumsum",
-#     "total_network_throughput",
-#     "spectral_efficiencies",
-#     "violations",
-#     "violations_cumsum",
-# ]
-metrics = ["violations", "violations_cumsum"]
+scenario_names = ["industrial"]
+agent_names = ["ssr_protect"]  # , "ssr"]
+metrics = [
+    "pkt_incoming",
+    "pkt_effective_thr",
+    "pkt_throughputs",
+    "dropped_pkts",
+    "buffer_occupancies",
+    "buffer_latencies",
+    "basestation_ue_assoc",
+    "basestation_slice_assoc",
+    "slice_ue_assoc",
+    "reward",
+    "reward_cumsum",
+    "total_network_throughput",
+    "spectral_efficiencies",
+    "violations",
+    "violations_cumsum",
+]
 episodes = np.array([0], dtype=int)
 slices = np.arange(3)
 
-gen_results(scenario_name, agent_name, episodes, metrics, slices)
+gen_results(scenario_names, agent_names, episodes, metrics, slices)
